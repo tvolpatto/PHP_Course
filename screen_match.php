8984acc427d82735f1a41a4c97bcee5e3e36ffcd
Thyago Volpatto <tvolpatto@gmail.com>
2026-04-21 21:46:57 -0600

    php course - variable and break lines

--- screen_match.php
+++ screen_match.php
@@ -1,5 +1,11 @@
 <?php
 
-echo "Welcome to the Screen match!";
+echo "Welcome to the Screen match!
+";
 
 $movieName =  "Top Gun - Maverick";
+$releaseYear = 2022;
+$movieScore = 8.8;
+$withThePlan = true;
+
+echo $movieName;
